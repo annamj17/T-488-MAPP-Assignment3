92bab6db300d3a5e181b68b808b1294c54725eb0
Arnar <arnara17@ru.is>
2019-12-10 19:44:53 +0000

    Removed unwanted chars v2

--- src/actions/cinemaActions.js
+++ src/actions/cinemaActions.js
@@ -2,19 +2,22 @@ import getAllCinemas from '../services/CinemaService';
 import * as constants from '../constants';
 
 export const getCinemas = () => {
-    return async dispatch => {
-        try {
-            const cinemas = await getAllCinemas();
-            dispatch(getcinemasSuccess(cinemas));
-        } catch (err) {
-            // TODO: Should dispatch an error action
-        }
-    };
+	return async dispatch => {
+		try {
+			const cinemas = await getAllCinemas();
+			let removeUnwantedCharacters = JSON.stringify(cinemas)
+			var cleanString = removeUnwantedCharacters.replace(/\\t|\\n|\\r|<[^>]+>/gm, '');
+			cinemaObject = JSON.parse(cleanString);
+			dispatch(getcinemasSuccess(cinemaObject));
+		} catch (err) {
+			// TODO: Should dispatch an error action
+		}
+	};
 }
 
 const getcinemasSuccess = cinemas => {
-    return {
-        type: constants.GET_CINEMAS,
-        payload: cinemas
-    };
+	return {
+		type: constants.GET_CINEMAS,
+		payload: cinemas
+	};
 };
