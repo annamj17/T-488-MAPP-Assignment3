ce09517e3d47d4560d0f5fae18ab5c11d16bb205
Arnar <arnara17@ru.is>
2019-12-11 14:11:21 +0000

    Added youtube video to movie details page

--- src/views/Movie/movieView.js
+++ src/views/Movie/movieView.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, WebView, Platform } from 'react-native';
 import { connect } from 'react-redux';
 import RenderAllMovieDetails from '../../components/RenderAllMovieDetails/RenderAllMovieDetails';
 
 const movieView = ({ pressedMovie }) => {
-    return (
-        <View>
-            <RenderAllMovieDetails pressedMovie={pressedMovie} />
-        </View>
-    )
+	console.log(pressedMovie.trailers[0].results[0].url);
+
+	return (
+		<View>
+			<View>
+				<RenderAllMovieDetails pressedMovie={pressedMovie} />
+			</View>
+			<View style={{ height: 300 }}>
+				<WebView
+					style={styles.WebViewContainer}
+					javaScriptEnabled={true}
+					domStorageEnabled={true}
+					source={{ uri: pressedMovie.trailers[0].results[0].url }}
+				/>
+			</View>
+		</View>
+	);
 }
 
 const mapStateToProps = (reduxStoreState, myProps) => {
-    const { movie } = reduxStoreState;
-    const { navigation } = myProps;
-    const movieIdent = navigation.getParam('id', 0);
-    const pressedMovie = movie.find(c => c.id === movieIdent)
-    return {
-        pressedMovie
-    }
+	const { movie } = reduxStoreState;
+	const { navigation } = myProps;
+	const movieIdent = navigation.getParam('id', 0);
+	const pressedMovie = movie.find(c => c.id === movieIdent)
+	return {
+		pressedMovie
+	}
 };
+
+const styles = StyleSheet.create({
+
+	WebViewContainer: {
+		marginTop: (Platform.OS == 'android') ? 20 : 0,
+	}
+});
 
 export default connect(mapStateToProps)(movieView);
